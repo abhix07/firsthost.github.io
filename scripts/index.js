a2e6085fb7637c7ae8b260be3c39c204ba1023c4
ABHISHEK PARMAR <101384942+abhix07@users.noreply.github.com>
2022-03-26 19:57:32 +0530

    Update index.js
    Change date

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'MARCH 26, 2022',
-  name: 'SAKSHI❤'
+  birthdate: 'MARCH 27, 2022',
+  name: 'SAKSHI'
 };
 
 function hideEverything() {
